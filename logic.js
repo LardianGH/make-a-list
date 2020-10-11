0df453c2c01a38dc625d33a4f203b7c8d301e357
Zion Brown <zabs95@gmail.com>
2020-10-11 10:13:55 -0400

    completed the basic app, from now on shall be quality of life changes and visual appeal changes.

--- logic.js
+++ logic.js
@@ -1,1 +1,10 @@
-alert("link start")
+const list = []
+const br = document.createElement("br");
+function addList() {
+    const newestInput = (list.length + 1) + " " + document.getElementById("textBox").value
+    list.push(newestInput)
+    console.log(list)
+    document.getElementById("listArea").innerHTML = list.join("<br>")
+    document.getElementById("textBox").value = ""
+    console.log(document.getElementById("listArea").innerHTML)
+}
