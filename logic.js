44661b4200834e7a13175caa4da6fb4bdf07a9fe
Zion Brown <zabs95@gmail.com>
2020-10-11 20:13:36 -0400

    Unique ids on each 'note', using that to change colors of notes (may or may not keep). Also changed backround colors.

--- logic.js
+++ logic.js
@@ -1,10 +1,17 @@
 const list = []
 const br = document.createElement("br");
 function addList() {
-    const newestInput = (list.length + 1) + " " + document.getElementById("textBox").value
+    if (document.getElementById("textBox").value !== "") {
+    const newestInput = '<div class="listSlot" id="color' + list.length + '","color">'+  +(list.length + 1) + ". " + document.getElementById("textBox").value + '</div>'
     list.push(newestInput)
     console.log(list)
-    document.getElementById("listArea").innerHTML = list.join("<br>")
+    document.getElementById("listArea").innerHTML = list.join("")
     document.getElementById("textBox").value = ""
     console.log(document.getElementById("listArea").innerHTML)
+} else {
+    document.getElementById("errorMessage").innerHTML = "you cant enter a blank space"
+    setTimeout(function(){
+        document.getElementById("errorMessage").innerHTML = ""
+    }, (1000 * 2.5))
+}
 }
